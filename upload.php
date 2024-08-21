82dc4e548677429ef59166d784de6c0a76b35ce9
Murilo <77314569+Revocable@users.noreply.github.com>
2024-08-21 10:20:32 -0300

    removido codigo de redimensionar (precisa refatorar)

--- upload.php
+++ upload.php
@@ -31,62 +31,18 @@ if ($oldImagePath) {
     unlink($oldImagePath[0]); // Remove apenas a primeira imagem encontrada (deve ser apenas uma)
 }
 
-// Função para redimensionar a imagem
-function resizeImage($file, $maxHeight) {
-    list($width, $height, $type) = getimagesize($file);
-
-    if ($height > $maxHeight) {
-        $ratio = $maxHeight / $height;
-        $newWidth = $width * $ratio;
-        $newHeight = $maxHeight;
-
-        $src = null;
-        switch ($type) {
-            case IMAGETYPE_JPEG:
-                $src = imagecreatefromjpeg($file);
-                break;
-            case IMAGETYPE_PNG:
-                $src = imagecreatefrompng($file);
-                break;
-            case IMAGETYPE_GIF:
-                $src = imagecreatefromgif($file);
-                break;
-            default:
-                return false;
-        }
-
-        $dst = imagecreatetruecolor($newWidth, $newHeight);
-        imagecopyresampled($dst, $src, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-
-        switch ($type) {
-            case IMAGETYPE_JPEG:
-                imagejpeg($dst, $file);
-                break;
-            case IMAGETYPE_PNG:
-                imagepng($dst, $file);
-                break;
-            case IMAGETYPE_GIF:
-                imagegif($dst, $file);
-                break;
-        }
-
-        imagedestroy($src);
-        imagedestroy($dst);
-    }
-
-    return true;
+// Move o arquivo do diretório temporário para o diretório de upload
+if (!move_uploaded_file($_FILES['file']['tmp_name'], $uploadPath)) {
+    returnJson(500, 'Erro ao salvar a foto de perfil');
 }
 
-// Move o arquivo do diretório temporário para o diretório de upload e redimensiona
-if (!move_uploaded_file($_FILES['file']['tmp_name'], $uploadPath) || !resizeImage($uploadPath, 500)) {
-    returnJson(500, 'Erro ao salvar ou redimensionar a foto de perfil');
-}
-
-$res = Usuario::salvarFotoPerfil($idUsuario, $uploadPath);
+// Atualiza o caminho da foto de perfil no banco de dados
+$relativePath = 'uploads/profile-pictures/' . $fileName;
+$res = Usuario::salvarFotoPerfil($idUsuario, $relativePath);
 
 if ($res) {
     // Retorna o caminho da foto de perfil para ser utilizado na interface
-    echo json_encode(['msg' => 'Foto de perfil enviada e atualizada com sucesso.', 'foto_perfil' => $uploadPath]);
+    echo json_encode(['msg' => 'Foto de perfil enviada e atualizada com sucesso.', 'foto_perfil' => $relativePath]);
 } else {
     returnJson(500, 'Erro ao atualizar a foto de perfil na base de dados.');
 }
